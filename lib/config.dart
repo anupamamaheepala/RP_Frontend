d21d4ed5d1df52934129eabfcc65a37807142844
Anupama Maheepala <127319824+anupamamaheepala@users.noreply.github.com>
2025-12-03 11:55:07 +0530

    config file updated

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,28 @@
-//  class Config {
+// //  class Config {
+// //   static const baseUrl = "https://rp-backend-production-290c.up.railway.app";
+// // }
+// class Config {
 //   static const baseUrl = "https://rp-backend-production-290c.up.railway.app";
 // }
-class Config {
-  static const baseUrl = "https://rp-backend-production-290c.up.railway.app";
-}
+
+
+
+//ANUPAMA
+//class Config {
+//  static const baseUrl = "https://rp-backend-production-afe6.up.railway.app";
+//}
+
+//SADITH
+//class Config {
+//  static const baseUrl = "https://rp-backend-production-c169.up.railway.app";
+//}
+
+//RASHINI
+//class Config {
+//  static const baseUrl = "https://rp-backend-production-ca77.up.railway.app";
+//}
+
+//HIMANSA
+//class Config {
+//  static const baseUrl = "https://rp-backend-production-2448.up.railway.app";
+//}
