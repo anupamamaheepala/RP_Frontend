59c728771913d50add7d65455c1a8f861e1cfa27
Dewnethmi <himansagunerathne@gmail.com>
2026-03-08 21:02:18 +0530

    Add grade3 learning plan and update ADHD tasks

--- lib/pages/adhd/grade3/task_stats.dart
+++ lib/pages/adhd/grade3/task_stats.dart
@@ -1,17 +1,34 @@
 class TaskStats {
-  static int totalCorrect = 0;
-  static int totalPremature = 0; // Measures Impulsivity
-  static int totalWrong = 0;      // Measures Inattention
+  static int totalCorrect   = 0;
+  static int totalPremature = 0;
+  static int totalWrong     = 0;
 
-  static void addStats(Map<String, int> sessionStats) {
-    totalCorrect += sessionStats['correct'] ?? 0;
-    totalPremature += sessionStats['premature'] ?? 0;
-    totalWrong += sessionStats['wrong'] ?? 0;
+  // NEW: per-task response times for CV calculation
+  static List<int> task1ResponseTimes = [];
+  static List<int> task2ResponseTimes = [];
+  static List<int> task3ResponseTimes = [];
+
+  static void addStats(Map<String, dynamic> sessionStats, {int taskNumber = 0}) {
+    totalCorrect   += (sessionStats['correct']   as int? ?? 0);
+    totalPremature += (sessionStats['premature'] as int? ?? 0);
+    totalWrong     += (sessionStats['wrong']     as int? ?? 0);
+
+    // Store response times by task
+    final rts = sessionStats['response_times_ms'] as List<int>? ?? [];
+    if (taskNumber == 1) task1ResponseTimes = rts;
+    if (taskNumber == 2) task2ResponseTimes = rts;
+    if (taskNumber == 3) task3ResponseTimes = rts;
   }
 
+  static List<int> get allResponseTimes =>
+      [...task1ResponseTimes, ...task2ResponseTimes, ...task3ResponseTimes];
+
   static void reset() {
-    totalCorrect = 0;
+    totalCorrect   = 0;
     totalPremature = 0;
-    totalWrong = 0;
+    totalWrong     = 0;
+    task1ResponseTimes = [];
+    task2ResponseTimes = [];
+    task3ResponseTimes = [];
   }
 }
